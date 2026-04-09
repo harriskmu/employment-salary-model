5808c130268cf08883337e69284aa793e3d13b99
harriskmu <32658086+harriskmu@users.noreply.github.com>
2026-04-09 11:38:44 +0700

    Add files via upload

---
+++ requirements.R
@@ -0,0 +1,17 @@
+# Run this once before opening analysis.Rmd
+
+packages <- c(
+  "glmnet",
+  "MASS",
+  "olsrr",
+  "lmtest",
+  "car",
+  "caret",
+  "dplyr",
+  "tidyr",
+  "ggplot2",
+  "scales",
+  "knitr"
+)
+
+install.packages(packages)
